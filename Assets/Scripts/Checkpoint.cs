7836e9aac3d5641b104420a7d80f28300c54cbfc
Carter Menary <48659414+craterTheBox@users.noreply.github.com>
2022-04-13 17:21:18 -0400

    [Upd] Added checkpoint sound

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Checkpoint : MonoBehaviour
 {
     [SerializeField] Transform respawnPoint;
+    [SerializeField] UnityEvent OnReachCheckpoint = null;
+    bool _firstTime = false;
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player")) {
+            if (!_firstTime)
+                OnReachCheckpoint.Invoke();
+            _firstTime = true;
             var resetter = other.GetComponent<DeathReset>();
             if (resetter ==null)
                 return;
             resetter.setResetPoint(respawnPoint);
+
         }
     }
 }
