49d01eb410b7b494d796c1d6c786ca8c6294235e
Synral <44206227+Synral@users.noreply.github.com>
2022-04-12 22:10:11 -0400

    [Update] Cinemachine Storyboard
    + Added fade in/out from black at start/end of level
    + Added CinemachineHelper to deal with camera movement and switching

---
+++ Assets/Scripts/CinemachineHelper.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class CinemachineHelper : MonoBehaviour {
+    [SerializeField] CinemachineStoryboard storyboard;
+    [SerializeField] float timeToFade = 5f;
+    [SerializeField] UnityEvent fadeInStartEvents;
+    [SerializeField] UnityEvent fadeInEndEvents;
+    [SerializeField] UnityEvent fadeOutStartEvents;
+    [SerializeField] UnityEvent fadeOutEndEvents;
+
+    private void OnEnable() {
+        fadeIn();
+    }
+
+    public void fadeIn() {
+        StartCoroutine(FadeIn());
+    }
+    public void fadeOut() {
+        StartCoroutine(FadeOut());
+    }
+    public void moveCamera(Transform target) {
+        StartCoroutine(MoveCamera(target));
+    }
+    public void teleportCamera(Transform target) {
+        transform.position = target.position;
+        transform.rotation = target.rotation;
+    }
+
+    IEnumerator FadeIn() {
+        fadeInStartEvents.Invoke();
+        while(storyboard.m_Alpha != 0f) {
+            storyboard.m_Alpha = Mathf.MoveTowards(storyboard.m_Alpha, 0f, Time.fixedDeltaTime / timeToFade);
+            yield return new WaitForFixedUpdate();
+        }
+        fadeInEndEvents.Invoke();
+    }
+    IEnumerator FadeOut() {
+        fadeOutStartEvents.Invoke();
+        while(storyboard.m_Alpha != 1f) {
+            storyboard.m_Alpha = Mathf.MoveTowards(storyboard.m_Alpha, 1f, Time.fixedDeltaTime / timeToFade);
+            yield return new WaitForFixedUpdate();
+        }
+        fadeOutEndEvents.Invoke();
+    }
+    IEnumerator MoveCamera(Transform target) {
+        while((transform.position - target.position).magnitude > 0.1f) {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, Vector3.Magnitude(target.position - transform.position) * Time.fixedDeltaTime);
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+}
