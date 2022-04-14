2219a2161427fc34d265a8edc786f4025109cc48
Marcus V <43864630+Mvine@users.noreply.github.com>
2022-04-14 05:20:13 -0400

    [Add] Working tutorial wo

--- Assets/Scripts/UITutorial.cs
+++ Assets/Scripts/UITutorial.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UITutorial : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TextMeshProUGUI tmpUI;
+    [SerializeField] private string displayText;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.gameObject.tag == "Player")
+        {
+            tmpUI.text = displayText;
+            tmpUI.gameObject.SetActive(true);
+
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit(Collider other)
     {
-        
+        if(other.gameObject.tag == "Player")
+        {
+            tmpUI.gameObject.SetActive(false);
+            tmpUI.text = "";
+        }
+
     }
 }
